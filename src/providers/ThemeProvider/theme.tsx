54f37132750cb029a10a4d21e8145745bd69a9b7
Maksym Kushchenko <m4kushchenko@gmail.com>
2023-09-10 12:55:24 +0300

    upd theme breakpoints sizes

--- src/providers/ThemeProvider/theme.tsx
+++ src/providers/ThemeProvider/theme.tsx
@@ -3,6 +3,15 @@ import { createTheme } from '@mui/material/styles';
 // A custom theme for this app
 const theme = createTheme({
   palette: {},
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1226,
+      xl: 1440,
+    },
+  },
 });
 
 export default theme;
