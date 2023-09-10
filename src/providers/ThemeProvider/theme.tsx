bdfe0d1f81b9befd9a0f48cc87f64d09af23d2f2
Maksym Kushchenko <m4kushchenko@gmail.com>
2023-09-10 14:32:06 +0300

    upd Typography theme options

--- src/providers/ThemeProvider/theme.tsx
+++ src/providers/ThemeProvider/theme.tsx
@@ -1,8 +1,71 @@
 import { createTheme } from '@mui/material/styles';
 
+declare module '@mui/material/styles' {
+  interface TypographyVariants {
+    copyright: React.CSSProperties;
+  }
+
+  // allow configuration using `createTheme`
+  interface TypographyVariantsOptions {
+    copyright?: React.CSSProperties;
+  }
+}
+
+// Update the Typography's variant prop options
+declare module '@mui/material/Typography' {
+  interface TypographyPropsVariantOverrides {
+    copyright: true;
+  }
+}
+
 // A custom theme for this app
 const theme = createTheme({
-  palette: {},
+  typography: {
+    fontSize: 14,
+    htmlFontSize: 16,
+    h1: {
+      fontWeight: 900,
+      fontSize: '6.33rem',
+      letterSpacing: 0,
+      lineHeight: 1.1,
+    },
+    h3: {
+      fontWeight: 800,
+      fontSize: '1.688rem',
+      letterSpacing: 0,
+      lineHeight: 1.1,
+    },
+    body1: {
+      fontWeight: 400,
+      fontSize: '1.125rem',
+      letterSpacing: 0,
+      lineHeight: 1.63,
+    },
+    body2: {
+      fontWeight: 500,
+      fontSize: '1rem',
+      letterSpacing: 0,
+      lineHeight: 1.63,
+    },
+    caption: {
+      fontWeight: 700,
+      fontSize: '0.844rem',
+      letterSpacing: 0,
+      lineHeight: 1.62,
+    },
+    copyright: {
+      fontWeight: 400,
+      fontSize: '0.781rem',
+      letterSpacing: 0,
+      lineHeight: 1.75,
+    },
+    button: {
+      fontWeight: 400,
+      fontSize: '1rem',
+      letterSpacing: 0.5,
+      lineHeight: 1.5,
+    },
+  },
   breakpoints: {
     values: {
       xs: 0,
